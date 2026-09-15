bc172d97e150baca3e57a6c704ec19e646511ec9
Arnau Gómez Farell <arnau.gomez@tiptap.dev>
2026-09-15 10:35:15 +0200

    fix: do not console log

--- src/app/api/server-read-selection/route.ts
+++ src/app/api/server-read-selection/route.ts
@@ -28,7 +28,6 @@ export async function POST(req: Request) {
     editorContext,
     tools: { tiptapRead: true, tiptapEdit: true, readSelection: true },
   });
-  console.log(JSON.stringify((toolsResponse)))
 
   const tools = Object.fromEntries(
     toolsResponse.tools.map((toolDef) => [
